39873cd40c49e5b197792b5c4988f8776c5c918e
Minakuchi0245 <kamilia3791@gmail.com>
2022-06-01 21:49:33 +0900

    初期設定

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_many :notes
+  has_many :comments
+  has_many :likes
+       
+  validates :nickname, presence: true
+         
+  validates :birth_date, presence: true
+       
+  encrypted_password = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: encrypted_password, message: 'は無効です 英字と数字の両方を含めてください'
+end
