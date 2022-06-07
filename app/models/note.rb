bb41ba8e37c84e67c1f41bc5dda34a06ae42a890
Minakuchi0245 <kamilia3791@gmail.com>
2022-06-07 21:20:06 +0900

    ビュー変更

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,13 @@
 class Note < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :theme
+  belongs_to :who
+
   belongs_to :user 
   has_many   :comments
   has_many   :likes
+  has_one_attached :image
 
   with_options presence: true do
     validates :title
